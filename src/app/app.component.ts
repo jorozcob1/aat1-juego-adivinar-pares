b31beaeb0274f5a274dd6a10fdbb8160cb48d9e5
jorozcob1 <jorozcob1@miumg.edu.gt>
2024-07-17 21:26:11 -0600

    readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,103 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ImgRandomService } from './services/img-random.service';
-import { HttpClientModule } from '@angular/common/http';
-
-interface Card {
-  id: number;
-  imageUrl: string;
-  isFlipped: boolean;
-  isMatched: boolean;
-}
+import { Component } from '@angular/core';
+import { ImagenComponent } from './imagen/imagen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
-  providers: [ImgRandomService],
+  imports: [ImagenComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  cards: Card[] = [];
-  flippedCards: Card[] = [];
-  attempts = 0;
-  isGameWon = false;
-
-  constructor(private imgRandom: ImgRandomService) {}
-
-  ngOnInit(): void {
-    this.loadImages();
-  }
-
-  loadImages(): void {
-    this.cards = [];
-    this.attempts = 0;
-    this.isGameWon = false;
-    for (let i = 0; i < 6; i++) {
-      this.imgRandom.obternerImagen().subscribe(
-        (data: any) => {
-          const card: Card = {
-            id: this.cards.length,
-            imageUrl: data.message,
-            isFlipped: false,
-            isMatched: false,
-          };
-          this.cards.push(card, { ...card, id: this.cards.length + 1 });
-          if (this.cards.length === 12) {
-            this.shuffleCards();
-          }
-        },
-        (error) => {
-          console.log('Error al obtener la imagen', error);
-        }
-      );
-    }
-  }
-
-  shuffleCards(): void {
-    for (let i = this.cards.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
-    }
-  }
-
-  flipCard(card: Card): void {
-    if (
-      this.isGameWon ||
-      card.isMatched ||
-      card.isFlipped ||
-      this.flippedCards.length === 2
-    )
-      return;
-
-    card.isFlipped = true;
-    this.flippedCards.push(card);
-
-    if (this.flippedCards.length === 2) {
-      this.attempts++;
-      setTimeout(() => this.checkMatch(), 1000);
-    }
-  }
-
-  checkMatch(): void {
-    const [card1, card2] = this.flippedCards;
-    if (card1.imageUrl === card2.imageUrl) {
-      card1.isMatched = card2.isMatched = true;
-      this.checkVictory();
-    } else {
-      card1.isFlipped = card2.isFlipped = false;
-    }
-    this.flippedCards = [];
-  }
-
-  checkVictory(): void {
-    if (this.cards.every((card) => card.isMatched)) {
-      this.isGameWon = true;
-    }
-  }
-
-  restartGame(): void {
-    this.loadImages();
-  }
+export class AppComponent {
+  title = 'Juego de Memoria';
 }
